0330edfcecf996f2fc317f3c62287f204aef1691
Sarah Julia Gushef <sarahgushef@gmail.com>
2019-04-04 17:19:42 +0700

    try all strings writings  techniques

---
+++ README.md
@@ -0,0 +1,11 @@
+#JAVASCRIPT EXERCISE
+
+This is 2nd Javascript exercise of Impact Byte Haku
+
+<!-- This is just an example to write more than one lines at once, in a box -->
+
+```
+I'm just trying to write more than one lines in once.
+Is it works?
+It works!!! Wow, magic
+```
---
+++ index.js
@@ -0,0 +1,20 @@
+console.log("Saya mau makan");
+
+alert("Mau makan ga?");
+
+console.log("Saya mau makan soto sapi");
+
+// Use 3 bactick (`) to write more than one line strings at once
+console.log(`Saya mau makan soto sapi
+Tapi saya takut gemuk
+Jadi saya mau makan salad saja
+`);
+
+// Use `Strings ${variable name}` to concate string + variable
+
+var name = "Eric Tabach";
+var age = 12;
+
+console.log(`
+His name is ${name}. He's ${age} years old
+`);
